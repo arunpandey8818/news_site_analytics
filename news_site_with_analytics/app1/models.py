104eadd0f5706f73d685de6f69f1855e1f3dea70
arunpandey8818 <arun.pandey@contify.com>
2015-02-06 11:52:47 +0530

    2nd_edit_commit

--- news_site_with_analytics/app1/models.py
+++ news_site_with_analytics/app1/models.py
@@ -1,77 +1,32 @@
 from django.contrib import admin
 from django.db import models
-
 from django.contrib.auth.models import User   
         
 
-class Pref(models.Model):
+class Tag(models.Model):
     name = models.CharField(max_length=200)
 
     def __unicode__(self):
         return self.name
-
-
-class UserProf(models.Model):
-    user = models.OneToOneField(User)
-    age = models.IntegerField()  
-    mobile_no = models.CharField(max_length=10)       
-    prefrences = models.ManyToManyField(Pref)
-
-    def __unicode__(self):
-       return self.user.username
 
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     age = models.IntegerField()  
-    mobile_no = models.CharField(max_length=10)
-class Pref(models.Model):
-    name = models.CharField(max_length=200)
+    mobile_no = models.CharField(max_length=10)       
+    prefrences = models.ManyToManyField(Tag)
+
     def __unicode__(self):
-        return self.name
-class Register(models.Model):
-    username = models.CharField(max_length=200)
-    passwrd = models.CharField(max_length=200)
-    cpasswrd = models.CharField(max_length=200) 
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    age = models.IntegerField()
-    mobile_no = models.IntegerField(max_length=254)
-    prefrences = models.ManyToManyField(Pref)
-    def __unicode__(self):
-        return self.username
-class Stories(models.Model):
-    title = models.CharField(max_length=300)
-    body = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
-    prefrence = models.ManyToManyField(Pref)
-    is_top_news = models.BooleanField()    
-    def __unicode__(self):
-        return self.title
-class UserLoginTrack(models.Model):
-    usertime = models.DateTimeField(auto_now_add=True)
-    user = models.OneToOneField(User)
-class UserLoginTrackNew(models.Model):
-    usertime = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User)    
-class UserStoryTrack(models.Model):
-    storytime = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User)
-    storyid = models.ForeignKey(Stories)
-class StoryTagTrack(models.Model):
-    tagtime = models.DateTimeField(auto_now_add=True)
-    storyid = models.ForeignKey(Stories)
-class StoryTagTrackNew(models.Model):
-    tagtime = models.DateTimeField(auto_now_add=True)
-    storyid = models.ForeignKey(Stories)
-    tagid = models.ManyToManyField(Pref)
+       return self.user.username
+
+
 
 
 class Story(models.Model):
     title = models.CharField(max_length=300)
     body = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
-    prefrence = models.ManyToManyField(Pref)
+    pub_date = models.DateField(auto_now_add=True)
+    prefrence = models.ManyToManyField(Tag)
     is_top_news = models.BooleanField()  
     counter = models.IntegerField()  
 
@@ -79,24 +34,24 @@ class Story(models.Model):
         return self.title
     
 
-class StoryTagTrackNewM(models.Model):
-    tagtime = models.DateTimeField(auto_now_add=True)
+class StoryTagTrack(models.Model):
+    tagtime = models.DateField(auto_now_add=True)
     storyid = models.ForeignKey(Story)
-    tagid = models.ManyToManyField(Pref)
+    tagid = models.ManyToManyField(Tag)
 
 
-class UserStoryTrackNewM(models.Model):
-    storytime = models.DateTimeField(auto_now_add=True)
+class UserStoryTrack(models.Model):
+    storytime = models.DateField(auto_now_add=True)
     user = models.ForeignKey(User)
     storyid = models.ForeignKey(Story)
 
 
-class UserLoginTrackNewM(models.Model):
-    usertime = models.DateTimeField(auto_now_add=True)
+class UserLoginTrack(models.Model):
+    usertime = models.DateField(auto_now_add=True)
     user = models.ForeignKey(User)    
     
 
 admin.site.register(Story)    
-admin.site.register(UserProf)
-admin.site.register(Pref)
+admin.site.register(UserProfile)
+admin.site.register(Tag)
 
--- news_site_with_analytics/app1/urls.py
+++ news_site_with_analytics/app1/urls.py
@@ -8,24 +8,19 @@ urlpatterns = patterns('app1.views',
     url(r'^index/home/(?P<storyId>\d+)/$', 'detail'),
 
     url(r'^index/home/$', 'home'),
-    url(r'^index/signup/home/$', 'home'),
-
+   
     url(r'^index/user_prof/$', 'user_prof'),
     url(r'^index/user_profile/$', 'user_profile'),
 
     url(r'^index/my_view/$', 'my_view'),
-    url(r'^index/signup/home/logout/$', 'logout_view'),
+    url(r'^index/logout/$', 'logout_view'),
  
     url(r'^index/home/detail/$', 'detail'),
-    url(r'^index/signup/home/detail/$', 'detail'),
-
+    
     url(r'^index/home/analytics/$', 'analytics'),
-    url(r'^index/signup/home/analytics/$', 'analytics'),
-
-    url(r'^index/signup/home/analytics/tagusage/$', 'tagusage'),
+    
     url(r'^index/home/analytics/tagusage/$', 'tagusage'),
 
-    url(r'^index/signup/home/analytics/useractivity/$', 'useractivity'),
     url(r'^index/home/analytics/useractivity/$', 'useractivity'),
     
 )
